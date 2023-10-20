f00228edb4c32e04bf7f83a07c5b71f44ed63da5
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-10-20 17:21:00 +0500

    Task4

---
+++ Tyuiu.BogdanovaSA.Sprint2.Task4.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BogdanovaSA.Sprint2.Task4.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BogdanovaSA.Sprint2.Task4.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7cf2340-eb06-491e-9892-5a5d128ad693")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.BogdanovaSA.Sprint2.Task4.V24/Program.cs
+++ Tyuiu.BogdanovaSA.Sprint2.Task4.V24/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.BogdanovaSA.Sprint2.Task4.V24.Lib;
 
 namespace Tyuiu.BogdanovaSA.Sprint2.Task4.V24
 {
@@ -10,6 +11,41 @@ namespace Tyuiu.BogdanovaSA.Sprint2.Task4.V24
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила Богданова СА ИСПб-23-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                         *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                          *");
+            Console.WriteLine("* Задание #4                                                                        *");
+            Console.WriteLine("* Вариант #24                                                                       *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("*                                          x        2         2                     *");
+            Console.WriteLine("*                                 ( y + 1 )        y  + cos(x)  + 9                 *");
+            Console.WriteLine("* Если x + 2 > y + 24, то z = x * ( ----- ), иначе -----------------                *");
+            Console.WriteLine("*                                 ( x + 2 )         2        2                      *");
+            Console.WriteLine("*                                                  x  - sin(y)  + 12                *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+            double x, y;
+
+            Console.WriteLine("Введите значение переменной X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+            double z = Math.Round(ds.Calculate(x, y), 3);
+            Console.WriteLine(z);
+            Console.ReadKey();
         }
+
     }
 }
